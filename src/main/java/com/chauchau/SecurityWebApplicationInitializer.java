5a938dc078d4e36e357df4bc7c5056ed66c67441
TienCGC8JAVA <nguyenmanhtien08081994@gmail.com>
2019-10-14 13:43:07 +0700

    spring security

---
+++ src/main/java/com/chauchau/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.chauchau;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer
+    extends AbstractSecurityWebApplicationInitializer {
+}
